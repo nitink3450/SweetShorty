a26156e40936555824eb4d7403151109dd9a8daf
nitin3450 <nitinkanhurkar2016@gmail.com>
2023-09-27 20:24:54 +0530

    fix:dark mode btn new animation

--- components/pages/DarkMode/index.tsx
+++ components/pages/DarkMode/index.tsx
@@ -22,16 +22,80 @@ export default function ThemeToggle({ setIsDarkModeOn, isDarkModeOn }: any) {
   };
 
   return (
-    <button
-      className="focus:outline-none dark-btn  p-3 rounded-full fixed right-8 bottom-8 hover:scale-125 transition ease-in-out duration-500 dark:bg-[#ffffff7d] bg-[#0000007d]"
-      onClick={handleDarkMode}
-      aria-label="Theme toggle"
-    >
-      {isDark() ? (
-        <BiSun color="black" size={20} />
-      ) : (
-        <BiMoon color="white" size={20} />
-      )}
-    </button>
+    <>
+      <button
+        className="focus:outline-none dark-btn  p-3 rounded-full fixed right-8 bottom-8 md:hover:scale-125 transition ease-in-out duration-500 dark:bg-[#7b1fa380] bg-[#191919] toggle"
+        onClick={handleDarkMode}
+        aria-label="Theme toggle"
+      >
+        {/* {isDark() ? (
+          <BiSun color="black" size={20} />
+        ) : (
+          <BiMoon color="white" size={20} />
+        )} */}
+        <input id="switch" className="" checked={isDark()} type="checkbox" />
+        <div className="dark_icon icon--moon text-[#ffd43b] transition-all duration-500 ease-in">
+          <svg
+            height="32"
+            width="32"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            className="AnimatedMoon"
+          >
+            <path
+              clip-rule="evenodd"
+              d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z"
+              fill-rule="evenodd"
+            ></path>
+          </svg>
+        </div>
+        <div className="dark_icon icon--sun text-white dark:text-[#ffd43b] transition-all duration-500 ease-in">
+          <svg
+            height="32"
+            width="32"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            className="AnimatedSun"
+          >
+            <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z"></path>
+          </svg>
+        </div>
+      </button>
+
+      {/* <div className="container fixed top-36">
+        <label className="toggle" htmlFor="switch">
+          <input id="switch" className="" type="checkbox" />
+          <div className="dark_icon icon--moon">
+            <svg
+              height="32"
+              width="32"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                clip-rule="evenodd"
+                d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z"
+                fill-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+
+          <div className="dark_icon icon--sun">
+            <svg
+              height="32"
+              width="32"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z"></path>
+            </svg>
+          </div>
+        </label>
+      </div> */}
+    </>
   );
 }
