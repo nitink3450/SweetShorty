05a34c3f48684fc60a59f5a4122276dd6090432c
nitin3450 <nitinkanhurkar2016@gmail.com>
2023-11-04 23:27:23 +0530

    fix:page preloader added

---
+++ components/global/common/Loader/Loader.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <>
+      <section className="c_wrapper">
+        <div className="container33"></div>
+      </section>
+    </>
+  );
+};
+
+export default Loading;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,10 +8,16 @@ import { ThemeProvider } from "next-themes";
 import AnimatedCursor from "react-animated-cursor";
 import ThemeToggle from "@/components/pages/DarkMode";
 import AppContext from "@/context/AppContext";
+import Loading from "@/components/global/common/Loader/Loader";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [isDarkModeOn, setIsDarkModeOn] = useState();
   const [isSignUp, setIsSignUp] = useState<boolean>(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
 
   useEffect(() => {
     AOS.init({
@@ -25,36 +31,44 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <ThemeProvider attribute="class">
-        <AppContext.Provider value={{ isSignUp, setIsSignUp }}>
-          <AnimatedCursor
-            innerSize={20}
-            outerSize={30}
-            color="48,197,155"
-            outerAlpha={0.2}
-            innerScale={0.7}
-            outerScale={5}
-            // clickables={[
-            //   "a",
-            //   'input[type="text"]',
-            //   'input[type="email"]',
-            //   'input[type="number"]',
-            //   'input[type="submit"]',
-            //   'input[type="image"]',
-            //   "label[for]",
-            //   "select",
-            //   "textarea",
-            //   "button",
-            //   ".link",
-            // ]}
-          />
-          <ThemeToggle
-            isDarkModeOn={isDarkModeOn}
-            setIsDarkModeOn={setIsDarkModeOn}
-          />
-          <Component {...pageProps} isDarkModeOn={isDarkModeOn} />
-        </AppContext.Provider>
-      </ThemeProvider>
+      {!loading ? (
+        <>
+          <ThemeProvider attribute="class">
+            <AppContext.Provider value={{ isSignUp, setIsSignUp }}>
+              <AnimatedCursor
+                innerSize={20}
+                outerSize={30}
+                color="48,197,155"
+                outerAlpha={0.2}
+                innerScale={0.7}
+                outerScale={5}
+                // clickables={[
+                //   "a",
+                //   'input[type="text"]',
+                //   'input[type="email"]',
+                //   'input[type="number"]',
+                //   'input[type="submit"]',
+                //   'input[type="image"]',
+                //   "label[for]",
+                //   "select",
+                //   "textarea",
+                //   "button",
+                //   ".link",
+                // ]}
+              />
+              <ThemeToggle
+                isDarkModeOn={isDarkModeOn}
+                setIsDarkModeOn={setIsDarkModeOn}
+              />
+              <Component {...pageProps} isDarkModeOn={isDarkModeOn} />
+            </AppContext.Provider>
+          </ThemeProvider>
+        </>
+      ) : (
+        <>
+          <Loading />
+        </>
+      )}
     </>
   );
 }
